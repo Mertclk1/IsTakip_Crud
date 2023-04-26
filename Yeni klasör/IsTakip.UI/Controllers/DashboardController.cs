3c62f9747c89d2dc43fd5692fc7039690a2fc581
Mertclk1 <hmert.clk@gmail.com>
2023-04-26 16:22:46 +0300

    Is takip projesi
    İş takip projesinin katmanları çıkılmış olup, Müşteri classının crud işlemleri gerçekleşmiştir. Aynı zamanda tamplate giyrilmesi de yapılmıştır.

---
+++ Yeni klasör/IsTakip.UI/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IsTakip.UI.Controllers
+{
+	public class DashboardController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult PratikSoru()
+		{
+			return View();
+		}
+	}
+}
